b6f06fa99362c969f4c74b40d66445defb27053d
Mark Davidovich <105949749+MarkDavidovich@users.noreply.github.com>
2024-02-07 15:41:41 +0200

    Last commit didn't work for some reason

--- js/utils.js
+++ js/utils.js
@@ -21,29 +21,8 @@ function getRandomColor() {
 //             if (j < 0 || j >= mat[0].length) continue
 //             if (i === rowIdx && j === colIdx) continue
 //             const cell = mat[i][j]
-//             if (cell.isMine) neighbors++ // adjusted for current project
+//             if (cell.isMine) neighbors++
 //         }
 //     }
 //     return neighbors
 // }
-
-function countNeighbors(mat, rowIdx, colIdx) {
-    var neighbors = 0
-    const numRows = mat.length
-    const numCols = mat[0].length
-
-    for (let i = rowIdx - 1; i <= rowIdx + 1; i++) {
-        if (i < 0 || i >= numRows) continue
-        for (let j = colIdx - 1; j <= colIdx + 1; j++) {
-            if (j < 0 || j >= numCols) continue
-            if (i === rowIdx && j === colIdx) continue
-
-            const cell = mat[i][j];
-            if (cell.isMine) {
-                neighbors++
-            }
-        }
-    }
-
-    return neighbors
-}
